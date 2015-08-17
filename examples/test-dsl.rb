505477b3dee83a6de1887d22560beb72ef1f6957
Ripta Pasay <github@r8y.org>
2015-08-17 00:21:04 -0700

    examples: test DSL

--- examples/test-dsl.rb
+++ examples/test-dsl.rb
@@ -3,4 +3,10 @@ require 'rubygems'
 require 'bundler'
 
 Bundler.setup
+
+$LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib')))
 require 'drydock'
+
+base = Drydock.from('gliderlabs/alpine', tag: '3.2', logs: STDERR)
+base.run('apk update')
+puts base.to_image_id
