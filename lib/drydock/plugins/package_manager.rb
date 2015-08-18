541c7bf14f13cdf398d90b8612ced74513237699
Ripta Pasay <github@r8y.org>
2015-08-18 15:31:53 -0700

    plugins/package_manager: abstract methods

--- lib/drydock/plugins/package_manager.rb
+++ lib/drydock/plugins/package_manager.rb
@@ -3,8 +3,24 @@ require_relative 'base'
 
 module Drydock
   module Plugins
+    class PackageManager < Base
 
-    class PackageManager < Base; end
+      def add(*pkgs)
+        raise NotImplementedError, '#add must be overridde in the subclass'
+      end
 
+      def clean
+        raise NotImplementedError, '#clean must be overridde in the subclass'
+      end
+
+      def remove(*pkgs)
+        raise NotImplementedError, '#remove must be overridde in the subclass'
+      end
+
+      def update
+        raise NotImplementedError, '#update must be overridde in the subclass'
+      end
+
+    end
   end
 end
