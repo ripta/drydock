ef511d8232fc9c4e7d16308197f066d250201c30
Ripta Pasay <github@r8y.org>
2015-08-18 17:01:11 -0700

    plugins/npm: fix options plucking

--- lib/drydock/plugins/npm.rb
+++ lib/drydock/plugins/npm.rb
@@ -5,7 +5,8 @@ module Drydock
   module Plugins
     class NPM < Base
 
-      def install(*pkgs, opts = {})
+      def install(*pkgs)
+        opts = pkgs.last.is_a?(Hash) ? pkgs.pop : {}
         flags = project.cli_flags(opts)
         project.run("npm install #{flags} #{pkgs.join(' ')}")
       end
