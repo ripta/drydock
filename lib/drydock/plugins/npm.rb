7d0838798ff6a4ce5cd450a49e6c9b23c28d37e4
Ripta Pasay <github@r8y.org>
2015-08-18 17:00:15 -0700

    plugins/npm: initial iteration

--- lib/drydock/plugins/npm.rb
+++ lib/drydock/plugins/npm.rb
@@ -4,6 +4,12 @@ require_relative 'base'
 module Drydock
   module Plugins
     class NPM < Base
+
+      def install(*pkgs, opts = {})
+        flags = project.cli_flags(opts)
+        project.run("npm install #{flags} #{pkgs.join(' ')}")
+      end
+
     end
   end
 end
