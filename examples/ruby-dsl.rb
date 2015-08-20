f8bbe5ded198747783fd835b5ce37c1ec2e5fedd
Ripta Pasay <github@r8y.org>
2015-08-19 18:41:10 -0700

    examples: shorter ruby-only DSL

---
+++ examples/ruby-dsl.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env drydock
+
+app_root   = '/app'
+build_root = '/build'
+
+from 'gliderlabs/alpine', '3.2'
+
+with Plugins::APK do |pkg|
+  pkg.update
+  pkg.upgrade
+  pkg.add 'ruby', 'ruby-dev'
+end
+
+download_once 'https://github.com/tianon/gosu/releases/download/1.3/gosu-amd64', '/bin/gosu', chmod: 0755
