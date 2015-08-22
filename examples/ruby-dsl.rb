d0fe35a14b8b538c2039aab2b94cbb80cede8bee
Ripta Pasay <github@r8y.org>
2015-08-21 22:30:16 -0700

    examples: install curl

--- examples/ruby-dsl.rb
+++ examples/ruby-dsl.rb
@@ -6,6 +6,7 @@ with Plugins::APK do |pkg|
   pkg.update
   pkg.upgrade
   pkg.add 'ruby', 'ruby-dev'
+  pkg.add 'curl', no_cache: true
 end
 
 download_once 'https://github.com/tianon/gosu/releases/download/1.3/gosu-amd64', '/bin/gosu', chmod: 0755
