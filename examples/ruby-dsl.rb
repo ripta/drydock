59a284534c6b42b98b4f7d5ffb463382463feb69
Ripta Pasay <github@r8y.org>
2015-08-22 02:10:26 -0700

    examples: testing delays and logs

--- examples/ruby-dsl.rb
+++ examples/ruby-dsl.rb
@@ -6,7 +6,9 @@ with Plugins::APK do |pkg|
   pkg.update
   pkg.upgrade
   pkg.add 'ruby', 'ruby-dev'
-  pkg.add 'curl', no_cache: true
+  pkg.add 'curl'
 end
 
 download_once 'https://github.com/tianon/gosu/releases/download/1.3/gosu-amd64', '/bin/gosu', chmod: 0755
+
+run 'sleep 2 && date && sleep 2 && date && sleep 2 && date && sleep 2 && date', no_cache: true
