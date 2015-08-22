7cd0554419b204b6a666f2a0be0abf53615f7997
Ripta Pasay <github@r8y.org>
2015-08-21 22:23:50 -0700

    stream_monitor: short format event handler

--- lib/drydock/stream_monitor.rb
+++ lib/drydock/stream_monitor.rb
@@ -4,6 +4,23 @@ module Drydock
 
     extend Forwardable
 
+    SHORT_FORMAT = Proc.new do |event, is_new, serial|
+      timestamp = Time.at(event.time)
+      time_string = timestamp.strftime('%H:%M:%S')
+      long_id = event.id.to_s
+      short_id = if long_id.include?(':') || long_id.include?('/')
+        long_id
+      else 
+        long_id.slice(0, 12)
+      end
+
+      if is_new
+        Drydock.logger.info "    --> #{short_id} #{event.status}"
+      else
+        Drydock.logger.debug "    --> #{short_id} #{event.status}"
+      end
+    end
+
     def_delegators :@thread, :join, :kill, :run
 
     def initialize(event_handler)
