10c91201de06f03ad3889eae3b9b16a0fd18e26a
Ripta Pasay <github@r8y.org>
2015-08-21 22:00:20 -0700

    runtime_options: options parser definition

--- lib/drydock/runtime_options.rb
+++ lib/drydock/runtime_options.rb
@@ -1,8 +1,48 @@
 
+require 'optparse'
+
 module Drydock
   class RuntimeOptions
 
     attr_accessor :includes, :log_level
+
+    def self.parse!(args)
+      opts = new
+
+      parser = OptionParser.new do |cfg|
+        cfg.banner = "Usage: $0 [options...] [drydock-filename]"
+
+        cfg.separator ''
+        cfg.separator 'Runtime / build options:'
+
+        cfg.on('-I', '--include PATH', 'Load custom plugins from PATH') do |path|
+          opts.includes << path
+        end
+        
+        cfg.separator ''
+        cfg.separator 'General options:'
+
+        cfg.on('-h', '--help', 'Show this help message') do
+          puts cfg
+          exit
+        end
+
+        cfg.on('-q', '--quiet', 'Run silently, except for errors') do |value|
+          opts.log_level = Logger::ERROR
+        end
+
+        cfg.on('-v', '--verbose', 'Run verbosely') do |value|
+          opts.log_level = Logger::DEBUG
+        end
+
+        cfg.on('-V', '--version', 'Show version') do
+          puts Drydock.banner
+        end
+      end
+
+      parser.parse!(args)
+      opts
+    end
 
     def initialize
       @includes  = []
