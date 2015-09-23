7cceaa179330fb62ed8f7bde70502072b2b13e5c
Ripta Pasay <github@r8y.org>
2015-09-23 01:45:40 -0700

    spec: more project ones

--- spec/drydock/project_spec.rb
+++ spec/drydock/project_spec.rb
@@ -4,6 +4,15 @@ RSpec.describe Drydock::Project do
   let(:project) { described_class.new }
   after(:each)  { project.destroy! }
 
+  it 'require a `from` before a `run`' do
+    expect { project.run('ls /') }.to raise_error(Drydock::InvalidInstructionError)
+  end
+
+  it 'require only one `from` per project' do
+    expect { project.from('alpine') }.not_to raise_error
+    expect { project.from('alpine') }.to raise_error(Drydock::InvalidInstructionError)
+  end
+
   it 'sets no extra information on image commit' do
     expect { project.from('alpine') }.not_to raise_error
     expect { project.run('ls /')    }.not_to raise_error
@@ -27,6 +36,13 @@ RSpec.describe Drydock::Project do
     image = Docker::Image.get(project.last_image.id)
     expect(image).not_to be_nil
     expect(image.info['Author']).to eq(author)
+  end
+
+  it 'build ID is incremented at every build step' do
+    expect(project.build_id).to eq('0')
+
+    expect { project.from('alpine') }.not_to raise_error
+    expect(project.build_id).to eq('1')
   end
 
 end
