c362961b632aca91e1321830e1423b4e4e2b61a9
Ripta Pasay <github@r8y.org>
2015-09-24 11:51:01 -0700

    file_manager: support for prepend_path option

--- lib/drydock/file_manager.rb
+++ lib/drydock/file_manager.rb
@@ -2,15 +2,19 @@
 module Drydock
   class FileManager
 
-    def self.find(path, ignorefile, recursive: true)
+    def self.find(path, ignorefile, prepend_path: false, recursive: true)
+      path = path.sub(%r{/$}, '')
+
       [].tap do |results|
         ::Find.find(path) do |subpath|
-          subpath = subpath.sub(/^#{path}\//, '')
+          subpath = subpath.sub(%r{^#{path}/}, '')
 
           Find.prune if ignorefile.match?(subpath)
 
           if File.directory?(subpath)
             Find.prune if path != subpath && !recursive
+          elsif prepend_path
+            results << File.join(path, subpath)
           else
             results << subpath
           end
