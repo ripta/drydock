8133386277469c7b838bad68326f43dfb2213bf8
Ripta Pasay <github@r8y.org>
2015-09-24 17:36:46 -0700

    spec: stream monitoring

--- spec/drydock/stream_monitor_spec.rb
+++ spec/drydock/stream_monitor_spec.rb
@@ -1,4 +1,10 @@
 
 RSpec.describe Drydock::StreamMonitor do
+
+  describe '#alive?' do
+    let(:events) { [] }
+    subject { described_class.new(lambda { |e| events << e }).alive? }
+    it { is_expected.to be_truthy }
+  end
   
 end
