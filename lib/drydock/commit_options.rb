a7b1ae15910e1f70e00478aeec458e145f37bfe3
Ripta Pasay <ripta@users.noreply.github.com>
2016-01-12 15:29:46 -0800

    commit_options: class docs

--- lib/drydock/commit_options.rb
+++ lib/drydock/commit_options.rb
@@ -1,12 +1,23 @@
 
 module Drydock
+  # Generate commit options to be provided to `Docker::Image#commit`.
   class CommitOptions
+
     attr_reader :opts
 
+    # @param [Hash] opts the options to transform to commit options; all options
+    #   are optional
+    # @option opts [String, Array] :command the command to list in the image
+    # @option opts [String] :entrypoint the entrypoint to list in the image
+    # @option opts [String] :author the name / email of the author of the image
+    # @option opts [String] :comment the comment to include in the result image
     def initialize(opts = {})
       @opts = opts
     end
 
+    # The hash to pass onto `Docker::Image#commit`.
+    #
+    # @return [Hash]
     def to_h
       {}.tap do |commit|
         if opts.key?(:command)
@@ -23,5 +34,6 @@ module Drydock
         commit[:comment] = opts.fetch(:comment, '') if opts.key?(:comment)
       end
     end
+
   end
 end
