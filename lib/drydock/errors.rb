1d09bce9a9baf98c88a79e202301c5a02f3ec0cb
Ripta Pasay <ripta@users.noreply.github.com>
2016-01-13 18:12:15 -0800

    errors: ICEE takes more information as a hash

--- lib/drydock/errors.rb
+++ lib/drydock/errors.rb
@@ -5,7 +5,26 @@ module Drydock
   class InvalidInstructionError < OperationError; end
 
   class ExecutionError < OperationError; end
-  class InvalidCommandExecutionError < ExecutionError; end
+
+  class InvalidCommandExecutionError < ExecutionError
+
+    def initialize(message: nil, container: nil, configuration: {})
+      super(message)
+      @data = {
+        container: container,
+        configuration: configuration
+      }
+    end
+
+    def configuration
+      @data[:configuration]
+    end
+
+    def container
+      @data[:container]
+    end
+
+  end
 
   class InsufficientVersionError < OperationError; end
 end
